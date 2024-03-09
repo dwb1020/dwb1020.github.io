a3ec5d68a95eeb9381ade98639c8972cc084555b
dwb1020 <162842364+dwb1020@users.noreply.github.com>
2024-03-09 17:15:08 -0500

    Create reference.js

---
+++ reference.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function Reference() {
+  return (
+    <footer>
+      Made using -
+      <a
+        href="https://developer.themoviedb.org/docs/getting-started"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        TMDB API
+      </a>
+      <p>By: David Blodgett, Peter Grieve, and Shane Woods</p>
+    </footer>
+  );
+}
